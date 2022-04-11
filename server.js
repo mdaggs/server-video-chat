dd210e501d7bcffd6349504eddba18c0e9d348f4
mdlumen <matthew.daggs@lumen.com>
2022-04-11 17:12:25 -0500

    Edits for Video Rendering

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
-const app = require('express')()
-const server = require('http').createServer(app)
-const cors = require('cors')
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
 
-const io = require('socket.io')(server,
-    {
-        cors:
-        {
-            origin:"*",
-            methods: ["Get", "POST"]
-        }
-    })
+const io = require('socket.io')(server, 
+    { 
+        cors: 
+        { 
+            origin: "*", 
+            methods: ["GET", "POST"]
+        }   
+    }
+);
 
-app.use(cors())
+app.use(cors());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000; 
 
 app.get("/", (req, res) => {
-    res.send("Tug Server is up!")
-})
+    res.send("Server is Up...");
+});
 
-io.on('connection', (socket) => {
-    socket.emit('me', socket.id)
+io.on('connection', (socket) =>{
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('callEnded')
-    })
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () =>{
+        socket.broadcast.emit('callEnded');
+    });
 
     socket.on('callUser', ({ userToCall, signalData, from, name }) => {
-        io.to(userToCall).emit('callUser', {signal: signalData, from, name })
-    })
+        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
 
     socket.on('answerCall', (data) => {
-        io.to(data.to).emit('callAccepted', data.signal)
-    })
-})
+        io.to(data.to).emit('callAccepted', data.signal);
+    });
+    
+});
 
-server.listen(PORT, () => console.log(`Server is Listening on Port: ${PORT}`))
+
+server.listen(PORT, ()=> console.log(`Server is listening on port ${PORT}`));
